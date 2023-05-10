787f599896d2a61bc1c475e0bb2d5c26d98e2029
tarcisiobannwart <81330027+tarcisiobannwart@users.noreply.github.com>
2023-05-10 13:19:32 -0300

    Update CameraLauncher.java
    fixando a versão em 32 manualmente

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -224,7 +224,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
     //--------------------------------------------------------------------------
 
     private String[] getPermissions(boolean storageOnly, int mediaType) {
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        if (android.os.Build.VERSION.SDK_INT >= 32) {
             if (storageOnly) {
                 switch (mediaType) {
                     case PICTURE:
